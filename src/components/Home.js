fcf5fa491a994fb4b19e3dc42a080e8f786a9da1
Rockyscode <anilsahu7377@gmail.com>
2023-11-17 00:34:20 +0530

    did the checkout

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,60 @@ import "./Home.css";
 import { homeImage } from "../accets/Images";
 import Product from "./Product";
 
+import { sunglass, flowervase, bag, phone } from "../accets/Images";
+
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
         <img src={homeImage} alt="" className="home__image" />
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title"
+            price={1345.99}
+            rating={5}
+            image={sunglass}
+          />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title phone "
+            price={145.99}
+            rating={4}
+            image={flowervase}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title phone "
+            price={145.99}
+            rating={3}
+            image={flowervase}
+          />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title phone "
+            price={145.99}
+            rating={2}
+            image={bag}
+          />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title phone "
+            price={1456.99}
+            rating={5}
+            image={phone}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            key={Math.floor(Math.random() * 1000) + 1}
+            title="A totallyh random title flowervase "
+            price={1545.99}
+            rating={4}
+            image={flowervase}
+          />
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import './Product.css'
-import { phone } from "../accets/Images"
+import React from "react";
+import "./Product.css";
+import { phone } from "../accets/Images";
 
-function Product() {
-    return (
-        <>  
-            <div className="product">
-                <div className="produt__info">
-                    <p>Title</p>
-                    <p className='product__price'>
-                        <small>$</small>
-                        <strong>30</strong>
-                    </p>
-                    <div className="product__rating">⭐⭐⭐⭐</div>
-                    <img src={phone} alt="" />
-                    <button>Add to Basket</button>
-                </div>
-            </div>
-        </>
-    )
+function Product({ title, price, rating, image }) {
+  return (
+    <>
+      <div className="product">
+        <div className="produt__info">
+          <p>{title}</p>
+          <p className="product__price">
+            <small>$</small>
+            <strong>{price}</strong>
+          </p>
+          <div className="product__rating">
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐</p>
+              ))}
+          </div>
+          <img src={image} alt={title} />
+          <button>Add to Basket</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Product
+export default Product;
